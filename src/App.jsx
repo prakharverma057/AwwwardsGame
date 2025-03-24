a9ad68c0b2c0e0ab8e442255730edb1ed68c4602
Prakhar Verma <prakharverma.work@gmail.com>
2025-03-24 20:04:39 +0530

    About and AnimatedTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Hero from "./Components/hero";
+import About from "./Components/About";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="z-0 min-h-screen bg-blue-500" />
+      <About/>
     </main>
   );
 }
