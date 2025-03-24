d3216c152cc0bad44b76a8a9e47e1df97ab432f2
Prakhar Verma <prakharverma.work@gmail.com>
2025-03-25 01:56:04 +0530

    AnimatedTitle and refractor About

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -28,9 +28,13 @@ const About = () => {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry{" "}
+          Welcome to Zentry
         </h2>
-        <AnimatedTitle />
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure"
+          containerClass="mt-5 !text-black text-center"
+        />
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
           <p>Zentry unites every player from countless games and platforms</p>
