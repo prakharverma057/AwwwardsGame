ae73dc7c99217e06fc36c15b13cb546f83cdb7ae
Prakhar Verma <prakharverma.work@gmail.com>
2025-04-11 21:20:08 +0530

    add Navbar component with audio control and scroll behavior

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Hero from "./Components/hero";
 import About from "./Components/About";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
-      <About/>
+      <About />
     </main>
   );
 }
